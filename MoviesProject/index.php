a34e904f3d0a6a1a5b9a61cf388e87f0cfdd895c
Ahmed Alhai <A7med_0@live.com>
2023-05-13 22:34:08 +0300

    Add files via upload
    Things changed:
     - Edited index.php as the homepage (still needs work)
     - Edited header.php to include navigation menu
     - Edited style.css has a few unused functions for later
     - Edited footer.html with simple text
     - Added sidebar.php which includes adding a search bar and other things like category selection
     - Added bootstrap for better css handling and makes website look prettier

--- MoviesProject/index.php
+++ MoviesProject/index.php
@@ -1,17 +1,80 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
+<html lang="en">
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>News Portal | Home Page</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- The Navigation Menu -->
+   <?php include 'header.php';?>
+
+    <!-- Whole Page Content -->
+    <div class="container">
         
-        ?>
-    </body>
+        <!-- Centering the Blog Posts -->
+      <div class="row" style="margin-top: 4%">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <!-- Blog Post Card-->
+          <div class="card mb-4">
+                      <img class="card-img-top" src="images/heat.png">
+            <div class="card-body">
+              <h2 class="card-title"> This is a Post Title Text </h2>
+              
+              <!--category-->
+              <p><a class="badge bg-secondary text-decoration-none link-light" href="" style="color:#fff"> This is the Category Text </a>
+                  
+              <!--Subcategory--->
+              <a class="badge bg-secondary text-decoration-none link-light"  style="color:#fff"> This is a Sub-Category Text </a></p>
+       
+              <a href="index.php" class="btn btn-primary"> Read More Buttonish &rarr;</a>
+            </div>
+              
+              <!-- Date Published -->
+            <div class="card-footer text-muted">
+              Posted on [Enter Date Here]
+            </div>
+          </div>  
+          <!-- Pagination Section for later -->
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+      <?php include 'sidebar.php';?>
+      </div>
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+      <?php include 'footer.html';?>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="bootstrap/jquery/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+ 
+</head>
+  </body>
+
 </html>
+
