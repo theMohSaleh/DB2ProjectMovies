7ebd645e66b52de8e4d497a02d0386875c817d41
Ahmed Alhai <A7med_0@live.com>
2023-05-20 23:07:27 +0300

    Add files via upload
    Changes from 'Homepage-Added' branch to 'main':
     - Bootstrap Folder added
     - footer.html added
     - header.php edited
     - Images Folder added
     - index.php moved the login function to a new php file 'loginPage.php'
     - sidebar.php added
     - style.css edited

---
+++ loginPage.php
@@ -0,0 +1,43 @@
+<?php
+
+include 'header.php';
+
+
+
+if (isset($_POST['submitted'])) {
+
+    $lgnObj = new Login();
+    $username = $_POST['Username'];
+    $password = $_POST['Password'];
+    
+    if ($lgnObj->login($username, $password)) {
+        header('Location: regiser.php');
+    } else {
+        echo $error = 'Wrong Login Values';
+    }  
+}
+include "header.html";
+?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <?php
+        if (empty($_SESSION["userID"])){
+         echo '<div><form action="" method="post">
+           <p><h1>Login Form</h1> 
+        <p>
+           <p>Username   <input type="text" name="Username" /></p>
+           <p>Password    <input type="password" name="Password" /></p>
+        </p>
+        <p><input type="submit" name="submit" value="Login" /></p>
+        
+         <input type ="hidden" name="submitted" value="TRUE">
+         </form></div>';   
+        }
+        ?>
+    </body>
+</html>
