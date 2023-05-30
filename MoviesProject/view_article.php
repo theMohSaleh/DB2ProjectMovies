bbbb9d2478615f36bf73e0612b843fcf79216eee
Ahmed Alhai <A7med_0@live.com>
2023-05-30 14:59:32 +0300

    Added back some removed stuff by accident
    Modified: view_article.php

--- MoviesProject/view_article.php
+++ MoviesProject/view_article.php
@@ -69,7 +69,16 @@ if (isset($_POST['commentPosted'])) {
     } else {
         echo "<script>alert('Error');</script>";
     }
-    
+
+    // redirect user to removed_article page if article was removed by an admin
+    if ($article->getTitle() == "*this article was removed by an administrator*") {
+        header('Location: removed_article.php');
+    }
+
+    // redirect user to home page if article is not published
+    if ($article->getIsPublished() == 0) {
+        header('Location: index.php');
+    }
 }
 ?>    
 <!DOCTYPE html>
